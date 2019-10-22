1b1b6e6a4f77eab9935b49a79ba8a746b82e6e34
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2019-10-22 15:09:47 -0300

    Finishing app search a repository and your informations

---
+++ README.md
@@ -0,0 +1,11 @@
+# Search a repository and save in storage
+
+This app will search a repository and get your information as owner of repository and the issues he has
+
+## Libs used
+
+React, React router dom, styled component, axios
+
+## Plugins used
+
+Eslint, prettier and editor config
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,103 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { FaGitAlt, FaPlus, FaSpinner } from 'react-icons/fa';
 
-export default function Main() {
-  return <h1>Hello main</h1>;
+import api from '../../services/api';
+
+import Container from '../../components/Container';
+
+import { Form, SubmitButton, List } from './styles';
+
+class Main extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      newRepo: '',
+      repositories: [],
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
+  handleChange = event => {
+    this.setState({ newRepo: event.target.value });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { repositories, newRepo, loading } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGitAlt color="#222" />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
 }
+
+export default Main;
